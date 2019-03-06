37913e2b0c91be0ee69176af568600b9f6676135
Andrej Byra <andrej.byra@synapse.com>
2019-03-06 16:22:02 +0100

    initial commit

---
+++ Singletone/Singletone/Singleton.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singletone
+{
+    class Singleton
+    {
+
+        public String name = "asas";
+
+        private Singleton() { }
+
+        private static Singleton singleton = null;
+
+        public static Singleton GetSingleton() {
+            if (singleton == null) {
+                singleton = new Singleton();
+                singleton.vrat();
+            }
+
+            return singleton;
+        }
+
+        public void vrat() {
+            
+            Console.WriteLine(name);
+
+
+
+        }
+
+       
+        
+
+
+
+
+
+
+
+
+
+
+    }
+}
